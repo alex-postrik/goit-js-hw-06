a90126bf75aed9e896bbfefddaa5ec59f8ab6f7e
Oleksandr Postryhan <112170360+alex-postrik@users.noreply.github.com>
2023-02-10 18:17:19 +0100

    task2

--- js/task-02.js
+++ js/task-02.js
@@ -7,3 +7,14 @@ const ingredients = [
   'Condiments',
 ];
 
+const inredientEl = document.querySelector('#ingredients');
+
+const itemEl = ingredients.map(ingredient => {
+  const itemListEl = document.createElement('li');
+  itemListEl.textContent = ingredient;
+   // console.log(itemListEl.textContent = ingredient);
+  itemListEl.classList.add('itemListEl');
+  return itemListEl;
+});
+inredientEl.append(...itemEl);
+console.log(inredientEl);
