5feed1f55811d35ff1f0b2a6e08ec01708d42654
Oleksandr Postryhan <112170360+alex-postrik@users.noreply.github.com>
2023-02-10 17:35:32 +0100

    task1

---
+++ js/task-01.js
@@ -0,0 +1,13 @@
+const categoryEL  = document.querySelector('#categories');
+console.log(`Number of categories: ${categoryEL.children.length}`);
+
+const itemEl = document.querySelectorAll('.item');
+itemEl.forEach(item =>  {
+    const title = item.querySelector('h2').textContent;
+    const count = item.querySelectorAll('li').length;
+    console.log(`Category: ${title}`);
+    console.log(`Elements: ${count}`);
+});
+
+
+
