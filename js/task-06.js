0773fd0fa1b95e403e2fa47c186a34456f4b7d48
Oleksandr Postryhan <112170360+alex-postrik@users.noreply.github.com>
2023-02-20 21:04:48 +0100

    6

--- js/task-06.js
+++ js/task-06.js
@@ -8,7 +8,7 @@ const inputEl = document.querySelector('#validation-input');
 // const validInput = document.querySelector("#validation-input");
 
 // inputEl.onInputBlur("blur", (evt) => {
-//   if (event.target.value.length == inputEl.getAttribute("data-length")) {
+//   if (evt.target.value.length == inputEl.getAttribute("data-length")) {
 //     inputEl.classList.add("invalid");
 //   } else {
 //     inputEl.classList.remove("invalid");
@@ -16,8 +16,8 @@ const inputEl = document.querySelector('#validation-input');
 //   }
 // });
 // ========================================================
-inputEl.addEventListener("blur", (event) => {
-  if (event.target.value.length == inputEl.getAttribute("data-length")) {
+inputEl.addEventListener("blur", (evt) => {
+  if (evt.target.value.length === inputEl.getAttribute("data-length")) {
     inputEl.classList.add("valid");
     if (inputEl.classList.contains("invalid")) {
       inputEl.classList.remove("invalid");
