bb0643e8e2a77bc168b12e9d45dd021449c803b5
Oleksandr Postryhan <112170360+alex-postrik@users.noreply.github.com>
2023-02-20 20:55:38 +0100

    task 6 -  corect

--- js/task-06.js
+++ js/task-06.js
@@ -1,34 +1,31 @@
 const inputEl = document.querySelector('#validation-input');
 // =========================================================
 
-inputEl.addEventListener('focus', onInputBlur);
+// inputEl.addEventListener("blur", onInputBlur);
 
 // =========================================================
- 
-function onInputBlur() {
-    if (inputEl.getAttribute('data-length') === inputEl.target.value.length) {
-        inputEl.classList.remove('invalid');
-    } else {
-        inputEl.classList.remove('invalid');
-        inputEl.classList.add('valid');
-    }
-};
+
+// const validInput = document.querySelector("#validation-input");
+
+// inputEl.onInputBlur("blur", (evt) => {
+//   if (event.target.value.length == inputEl.getAttribute("data-length")) {
+//     inputEl.classList.add("invalid");
+//   } else {
+//     inputEl.classList.remove("invalid");
+//     inputEl.classList.add("valid");
+//   }
+// });
 // ========================================================
-
-// const textInput = document.querySelector('#validation-input')
-
-// console.log(textInput.getAttribute('data-length'))
-
-// textInput.addEventListener('blur', event => {
-// 	if (event.target.value.length == textInput.getAttribute('data-length')) {
-// 		textInput.classList.add('valid')
-// 		if (textInput.classList.contains('invalid')) {
-// 			textInput.classList.remove('invalid')
-// 		}
-// 	} else {
-// 		if (textInput.classList.contains('valid')) {
-// 			textInput.classList.remove('valid')
-// 		}
-// 		textInput.classList.add('invalid')
-// 	}
-// })
+inputEl.addEventListener("blur", (event) => {
+  if (event.target.value.length == inputEl.getAttribute("data-length")) {
+    inputEl.classList.add("valid");
+    if (inputEl.classList.contains("invalid")) {
+      inputEl.classList.remove("invalid");
+    }
+  } else {
+    if (inputEl.classList.contains("valid")) {
+      inputEl.classList.remove("valid");
+    }
+    inputEl.classList.add("invalid");
+  }
+});
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,78 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+// const ref = {
+//   divControls: document.querySelector("#controls"),
+//   divBoxes: document.querySelector("#boxes"),
+// };
+// const div = createElement("div");
+// div.style.width = "30";
+// div.style.height = "30";
+// div.style.backround = getRandomHexColor();
+
+// ref.divControls.addEventListener("click", createBoxes);
+
+// function createBoxes(amount) {
+//   amount.prevenrtDefault();
+// }
+
+const ref = {
+    boxesRef: document.querySelector("#boxes"),
+    inputRef: document.querySelector("input"),
+    buttonRef: document.querySelectorAll("button"),
+    buttonCreateRef: document.querySelector("[data-create]"),
+    buttonDestroyeRef: document.querySelector("[data-destroy]"),
+};
+// console.log(boxesRef);
+// console.log(inputRef);
+// console.log(buttonRef);
+
+// ref.buttonCreateRef.addEventListener("click", createBoxes);
+
+const createBoxes = (amount) => {
+    let defaultSize = 30;
+    const elemDiv = [];
+    for (let i = 1; i <= amount; i += 1) {
+
+        const divEl = document.createElement("div");
+        divEl.style.backgroundColor = getRandomHexColor();
+        divEl.style.width = 30 + "px";
+        divEl.style.height = 30 + "px";
+
+        elements.push(divEl);
+    };
+
+    ref.boxesRef.append(...elemDiv);
+};
+
+ref.buttonCreateRef.addEventListener("click", createBoxes);
+
+// const destroyBoxes = () => {
+//   ref.boxesRef.innerHTML = "";
+// };
+// =======================================================
+// const createBoxes = (amount) => {
+//   let defaultSize = 30;
+//   const arrayDiv = [];
+
+//   for (let i = 0; i < amount; i++) {
+//     let randomColor = getRandomHexColor();
+//     defaultSize += 10;
+
+//     const newDivEl = document.createElement("div");
+//     newDivEl.style.margin = "10px";
+//     newDivEl.style.width = defaultSize + "px";
+//     newDivEl.style.height = defaultSize + "px";
+//     newDivEl.style.backgroundColor = randomColor;
+
+//     arrayDiv.push(newDivEl);
+//   }
+//   return insertDivBoxesEl.append(...arrayDiv);
+// };
+// const onCreateBoxes = (event) => createBoxes(inputEl.value);
+// const onDestroyBoxes = (event) => (
+//   (insertDivBoxesEl.innerHTML = ""), (inputEl.value = "")
+// );
+
+// ref.buttonCreateRef.addEventListener("click", onCreateBoxes);
+// ref.buttonDestroyeRef.addEventListener("click", onDestroyBoxes);
